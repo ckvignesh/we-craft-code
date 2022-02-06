c258136bc61c7d701474dc3c2a1edfc577782c68
Eduardo Vedes <eduardo.vedes@gmail.com>
2022-02-06 16:56:25 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# we-craft-code
-wecraftcode.org homepage
+# wecraftcode.org homepage
+
+We're a small group of people who leverage learning and programming through motivation, mentoring, and craftsmanship.
+
+2022
---
+++ app/routes/index.tsx
@@ -0,0 +1,19 @@
+export default function Index() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "black",
+        height: "100vh",
+      }}
+    >
+      <img
+        src="/images/logo_transparent_background.png"
+        style={{ maxWidth: "80%" }}
+      />
+    </div>
+  );
+}
